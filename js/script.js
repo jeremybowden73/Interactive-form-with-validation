fbbf39cb8cd2ded2e2c57c168831e6d70d267f87
jeremybowden73 <jeremybowden73.gmail.com>
2018-06-01 11:08:55 +0100

    Revised style.css to show 'other job role' text field, and sscript.js to hide text fields if JS is enabled.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 //
+const otherJobRoleInput = document.getElementById("otherJobRole");
+otherJobRoleInput.style.display = "none";
+const creditCardInputDiv = document.getElementById("credit-card");
+creditCardInputDiv.style.display = "none";
+const paypalDiv = document.getElementById("paypal");
+paypalDiv.style.display = "none";
+const bitcoinDiv = document.getElementById("bitcoin");
+bitcoinDiv.style.display = "none";
